3ed8ed3771d61c15555978abc006d69dbea819f8
Fernando Binasco <fernando.binasco@gmail.com>
2021-07-19 15:33:47 -0300

    imagens carregar suavemente

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
 
@@ -6,6 +7,8 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 // command: flutter pub add share
 import 'package:share/share.dart';
+// command: flutter pub add transparent_image
+import 'package:transparent_image/transparent_image.dart';
 
 import 'gif_page.dart';
 
@@ -51,8 +54,9 @@ class _HomePageState extends State<HomePage> {
         itemBuilder: (context, index) {
           if (_search == '' || index < snapshot.data["data"].length) {
             return GestureDetector(
-              child: Image.network(
-                snapshot.data["data"][index]["images"]["fixed_height"]["url"],
+              child: FadeInImage.memoryNetwork(
+                placeholder: kTransparentImage,
+                image: snapshot.data["data"][index]["images"]["fixed_height"]["url"],
                 height: 300.0,
                 fit: BoxFit.cover,
               ),
