7a16b925ec38d0501455816c404eafa440ec5812
Fernando Binasco <fernando.binasco@gmail.com>
2021-07-19 14:00:07 -0300

    carregando mais gifs

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -21,32 +21,64 @@ class _HomePageState extends State<HomePage> {
 
     if (_search == '') {
       response = await http.get(Uri.parse(
-          "https://api.giphy.com/v1/gifs/trending?api_key=rIAEZNO64UmELaJvG8yabVAVWXFW2Qmi&limit=20&rating=g"));
+          "https://api.giphy.com/v1/gifs/trending?api_key=rIAEZNO64UmELaJvG8yabVAVWXFW2Qmi&limit=19&rating=g"));
     } else {
       response = await http.get(Uri.parse(
-          "https://api.giphy.com/v1/gifs/search?api_key=rIAEZNO64UmELaJvG8yabVAVWXFW2Qmi&q=$_search&limit=20&offset=$_offset&rating=g&lang=pt"));
+          "https://api.giphy.com/v1/gifs/search?api_key=rIAEZNO64UmELaJvG8yabVAVWXFW2Qmi&q=$_search&limit=19&offset=$_offset&rating=g&lang=pt"));
     }
 
     return json.decode(response.body);
   }
 
-  Widget _createGifTable(BuildContext context, AsyncSnapshot snapshot){
+  int _getCount(List data) {
+    if (_search == '') {
+      return data.length;
+    } else {
+      return data.length + 1;
+    }
+  }
+
+  Widget _createGifTable(BuildContext context, AsyncSnapshot snapshot) {
     return GridView.builder(
         padding: EdgeInsets.all(10.0),
         gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          crossAxisSpacing: 10.0,
-          mainAxisSpacing: 10.0
-        ), 
-        itemCount: snapshot.data["data"].length,
-        itemBuilder: (context, index){
-          return GestureDetector(
-            child: Image.network(snapshot.data["data"][index]["images"]["fixed_height"]["url"],
-            height: 300.0,
-            fit: BoxFit.cover,),
-          );
-        }
-    );
+            crossAxisCount: 2, crossAxisSpacing: 10.0, mainAxisSpacing: 10.0),
+        itemCount: _getCount(snapshot.data["data"]),
+        itemBuilder: (context, index) {
+          if (_search == '' || index < snapshot.data["data"].length) {
+            return GestureDetector(
+              child: Image.network(
+                snapshot.data["data"][index]["images"]["fixed_height"]["url"],
+                height: 300.0,
+                fit: BoxFit.cover,
+              ),
+            );
+          } else {
+            return Container(
+              child: GestureDetector(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.add,
+                      color: Colors.white,
+                      size: 70.0,
+                    ),
+                    Text(
+                      "Carregar mais...",
+                      style: TextStyle(color: Colors.white, fontSize: 22.0),
+                    )
+                  ],
+                ),
+                onTap: (){
+                  setState(() {
+                    _offset += 19;
+                  });
+                },
+              ),
+            );
+          }
+        });
   }
 
   @override
@@ -79,12 +111,19 @@ class _HomePageState extends State<HomePage> {
                   border: OutlineInputBorder()),
               style: TextStyle(color: Colors.white, fontSize: 18.0),
               textAlign: TextAlign.center,
+              onSubmitted: (text) {
+                setState(() {
+                  _search = text;
+                  _offset = 0;
+                });
+              },
             ),
           ),
-          Expanded(child: FutureBuilder(
+          Expanded(
+              child: FutureBuilder(
             future: _getGifs(),
-            builder: (context, snapshot){
-              switch(snapshot.connectionState){
+            builder: (context, snapshot) {
+              switch (snapshot.connectionState) {
                 case ConnectionState.waiting:
                 case ConnectionState.none:
                   return Container(
@@ -97,7 +136,7 @@ class _HomePageState extends State<HomePage> {
                     ),
                   );
                 default:
-                  if(snapshot.hasError) {
+                  if (snapshot.hasError) {
                     return Container();
                   } else {
                     return _createGifTable(context, snapshot);
@@ -108,7 +147,5 @@ class _HomePageState extends State<HomePage> {
         ],
       ),
     );
-
-
   }
 }
